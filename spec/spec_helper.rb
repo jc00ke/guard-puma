1dc419aad7a9dce3a81a23fbfc20c0298e2fdf01
jc00ke <jesse@jc00ke.com>
2014-11-11 14:42:14 -0800

    Remove deprecated RSpec config setting

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -8,7 +8,6 @@ ENV["GUARD_ENV"] = 'test'
 require 'rspec'
 
 RSpec.configure do |config|
-  config.treat_symbols_as_metadata_keys_with_true_values = true
   config.run_all_when_everything_filtered = true
   config.filter_run :focus
   config.mock_with :rspec
