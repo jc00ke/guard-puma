194f7449d33b9ac5423ae87e980a9fe197a6e8c8
Ian Ker-Seymer <i.kerseymer@gmail.com>
2020-09-30 10:23:00 -0400

    Update --control to --control-url

--- spec/lib/guard/puma/runner_spec.rb
+++ spec/lib/guard/puma/runner_spec.rb
@@ -18,7 +18,7 @@ describe Guard::PumaRunner do
       runner = described_class.new(options.merge(control_token: "abc"))
       expect(runner.cmd_opts).to include("--config - ")
       expect(runner.cmd_opts).to include("--control-token abc")
-      expect(runner.cmd_opts).to include("--control tcp://localhost:9293")
+      expect(runner.cmd_opts).to include("--control-url tcp://localhost:9293")
       expect(runner.cmd_opts).to include("--port 4000")
       expect(runner.cmd_opts).to include("--environment development")
       expect(runner.cmd_opts).to include("--quiet")
@@ -132,7 +132,7 @@ describe Guard::PumaRunner do
           it "assumes options are set in config" do
             expect(runner.cmd_opts).to match("--config #{path}")
             expect(runner.cmd_opts).to match(/--control-token [0-9a-f]{10,}/)
-            expect(runner.cmd_opts).to match("--control tcp")
+            expect(runner.cmd_opts).to match("--control-url tcp")
             expect(runner.cmd_opts).to match("--environment #{environment}")
           end
         end
